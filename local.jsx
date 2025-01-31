b08c0c4e5578c38cff78a5423431cb3d3985eafc
Jhulia Kevelyn <jhuliakevelyn@gmail.com>
2025-01-31 16:36:13 -0300

    Create local.jsx

---
+++ local.jsx
@@ -0,0 +1,108 @@
+
+{/* import "./App.css";
+
+function App() {
+  const users = [
+    {
+      id: 1,
+      userName: "Adriel",
+      gender: "men",
+      description:
+        "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Deleniti sunt tempore, dicta distinctio, at optio voluptatem sapiente hic totam ipsum laborum maxime. Nihil, atque dolores qui in consequatur quasi quo.",
+      imageUrl:
+        "https://www.gettyimages.com.br/detail/foto/smiling-businessman-with-brown-bag-walking-in-city-imagem-royalty-free/691910455?searchscope=image%2Cfilm",
+      imageSize: 30,
+    },
+    {
+      id: 2,
+      userName: "Bernardo",
+      gender: "men",
+      description:
+        "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Deleniti sunt tempore, dicta distinctio, at optio voluptatem sapiente hic totam ipsum laborum maxime. Nihil, atque dolores qui in consequatur quasi quo.",
+      imageUrl:
+        "https://www.gettyimages.com.br/detail/foto/portrait-of-a-smiling-young-businessman-imagem-royalty-free/1297832726?searchscope=image%2Cfilm",
+      imageSize: 30,
+    },
+
+    {
+      id: 3,
+      userName: "Caio",
+      gender: "men",
+      description:
+        "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Deleniti sunt tempore, dicta distinctio, at optio voluptatem sapiente hic totam ipsum laborum maxime. Nihil, atque dolores qui in consequatur quasi quo.",
+      imageUrl:
+        "https://www.gettyimages.com.br/detail/foto/enhancing-his-entrepreneurial-ambition-with-the-imagem-royalty-free/637232624?searchscope=image%2Cfilm",
+      imageSize: 30,
+    },
+
+    {
+      id: 4,
+      userName: "Danilo",
+      gender: "masculine",
+      description:
+        "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Deleniti sunt tempore, dicta distinctio, at optio voluptatem sapiente hic totam ipsum laborum maxime. Nihil, atque dolores qui in consequatur quasi quo.",
+      imageUrl:
+        "https://www.gettyimages.com.br/detail/foto/happy-man-social-media-phone-and-living-room-relax-imagem-royalty-free/1440149723?searchscope=image%2Cfilm",
+      imageSize: 30,
+    },
+
+    {
+      id: 5,
+      userName: "Estevan",
+      gender: "masculine",
+      description:
+        "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Deleniti sunt tempore, dicta distinctio, at optio voluptatem sapiente hic totam ipsum laborum maxime. Nihil, atque dolores qui in consequatur quasi quo.",
+      imageUrl:
+        "https://www.gettyimages.com.br/detail/foto/businessman-using-digital-tablet-imagem-royalty-free/1184196223?searchscope=image%2Cfilm",
+      imageSize: 30,
+    },
+  ];
+
+  //está renderizando como eu esperada {} para javascript dentro de return que significa que é javascript dentro de html
+  return (
+    <>
+      <div>
+        <input type="radio" name="animal" value="dog" />
+        <p>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Deleniti
+          sunt tempore, dicta distinctio, at optio voluptatem sapiente hic totam
+          ipsum laborum maxime. Nihil, atque dolores qui in consequatur quasi
+          quo.
+        </p>
+        <ul>
+          {users.map((user) => (
+            <li key={user.id}>
+              <h2>{user.userName}</h2>
+            </li>
+          ))}
+          ,
+          {users.map((user) => (
+            <li key={user.id}>
+              <h2>{user.gender}</h2>
+            </li>
+          ))}
+          ,
+          {users.map((user) => (
+            <li key={user.id}>
+              <h2>{user.description}</h2>
+            </li>
+          ))}
+          ,
+          {users.map((user) => (
+            <li key={user.id}>
+              <h2>{user.imageUrl}</h2>
+            </li>
+          ))}
+          ,
+          {users.map((user) => (
+            <li key={user.id}>
+              <h2>{user.imageSize}</h2>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </>
+  );
+}
+export default App;
+*/}
